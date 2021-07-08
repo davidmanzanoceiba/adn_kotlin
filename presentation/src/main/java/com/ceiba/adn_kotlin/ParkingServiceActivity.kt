186aa6d049d734642aff064ef80234022a5a180e
David Manzano <david.manzano@ceiba.com.co>
2021-07-08 16:15:22 -0500

    Se renombra activity principal

--- presentation/src/main/java/com/ceiba/adn_kotlin/ParkingServiceActivity.kt
+++ presentation/src/main/java/com/ceiba/adn_kotlin/ParkingServiceActivity.kt
@@ -3,9 +3,9 @@ package com.ceiba.adn_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ParkingServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_parking_service)
     }
 }
